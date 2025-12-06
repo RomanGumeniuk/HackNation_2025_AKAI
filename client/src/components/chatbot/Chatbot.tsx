a08174e04d8afc9da3f9b100dbd6d49ca6abb9e4
David Tovmasyan <dtovmasyan2022@gmail.com>
2025-12-06 18:00:14 +0100

    Chatbot component added

---
+++ client/src/components/chatbot/Chatbot.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import {useState} from "react";
+import ChatbotIcon from "@/components/chatbot/ChatbotIcon";
+import ChatbotWindow from "@/components/chatbot/ChatbotWindow";
+
+export default function Chatbot() {
+
+    const [windowClosed, setWindowsClosed] = useState<boolean>(false);
+
+    const openChatWindow = () => {
+        setWindowsClosed(true);
+    }
+
+    const closeChatWindow = () => {
+        setWindowsClosed(false);
+    }
+
+    return (
+        <>
+            <ChatbotIcon isClosed={windowClosed} openChatWindow={openChatWindow} />
+            <ChatbotWindow isClosed={windowClosed} closeChatWindow={closeChatWindow} />
+        </>
+    )
+}
