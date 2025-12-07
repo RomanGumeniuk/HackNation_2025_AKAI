1e542d442509b8d85cd00097f23b9dcc6a26fe87
Wojciech <wojciech.stuchlak@gmail.com>
2025-12-07 01:36:30 +0100

    added 3 compontents i strona glowna ma tekst tabelki i jakis maly napis co ona robi

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,58 +1,64 @@
 import { lawsData } from '@/mock_data/laws';
 import { BookmarkCheck } from 'lucide-react';
+import CountUp from '@/components/CountUp';
+import SplitText from '@/components/SplitText';
 
 export default function Home() {
     return (
+        <>
+            <div className="flex items-center justify-center min-h-[50vh]">
+                <SplitText className='text-[3rem] font-bold' text='Wszystkie ustawy na wyciągnięcie myszki' delay={100}    duration={0.8}    ease="power3.out"    splitType="lines" />
+            </div>
 
+            <div>
+                <hr className="border-t border-gray-300 my-4" />
+                <h2 className="text-center text-lg text-gray-700">Poznaj najnowsze zmiany w prawie krajowym. Przeglądaj, śledź i bądź na bieżąco z aktualizacjami ustaw, które kształtują naszą rzeczywistość.</h2>
+                <hr className="border-t border-gray-300 my-4" />
+            </div>
+                
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 m-8">
+                <div>
+                    <h2 className="text-2xl font-bold mb-4">Najgorętsze ustawy</h2>
+                    <table className="w-full border-collapse border border-gray-300">
+                    <thead className="bg-gray-200">
+                        <tr>
+                        <th className="border border-gray-300 p-2 text-left">Nazwa</th>
+                        <th className="border border-gray-300 p-2 text-left">Aktualizacja</th>
+                        <th className="border border-gray-300 p-2 text-left"><BookmarkCheck size={24}/></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {lawsData.sort((a, b) => b.follows - a.follows).slice(0, 5).map((law) => (
+                        <tr key={law.id}>
+                            <td className="border border-gray-300 p-2"><a href={`/ustawu/${law.id}`} className="text-blue-600 hover:underline">{law.name}</a></td>
+                            <td className="border border-gray-300 p-2">{law.lastUpdate}</td>
+                            <td className="border border-gray-300 p-2"><CountUp   from={0}   separator=","    direction="up"    duration={1}    className="count-up-text" to={law.follows} /></td>
+
+                        </tr>
+                        ))}
+                    </tbody>
+                    </table>
+                </div>
+                <div>
+                    <h2 className="text-2xl font-bold mb-4">Ostatnie aktualizacje</h2>
+                    <table className="w-full border-collapse border border-gray-300">
+                    <thead className="bg-gray-200">
+                        <tr>
+                        <th className="border border-gray-300 p-2 text-left">Nazwa</th>
+                        <th className="border border-gray-300 p-2 text-left">Aktualizacja</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {lawsData.sort((a, b) => new Date(b.lastUpdate).getTime() - new Date(a.lastUpdate).getTime()).slice(0, 5).map((law) => (
+                        <tr key={law.id}>
+                            <td className="border border-gray-300 p-2"><a href={`/ustawu/${law.id}`} className="text-blue-600 hover:underline">{law.name}</a></td>
+                            <td className="border border-gray-300 p-2">{law.lastUpdate}</td>
+                        </tr>
+                        ))}
+                    </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
         );
 }
-
-
-        // <>
-        //     <div className="flex justify-center items-center m-8 h-full">
-        //     <h1 className="text-[2rem] font-bold">Witaj w Obywatelu Prawa zobacz najnowsze aktualizacje dotyczące prawa w polsce i nie tylko!</h1>
-        //     </div>
-        //     <div className="grid grid-cols-1 md:grid-cols-2 gap-8 m-8">
-        //     <div>
-        //         <h2 className="text-2xl font-bold mb-4">Najgorętsze ustawy</h2>
-        //         <table className="w-full border-collapse border border-gray-300">
-        //         <thead className="bg-gray-200">
-        //             <tr>
-        //             <th className="border border-gray-300 p-2 text-left">Nazwa</th>
-        //             <th className="border border-gray-300 p-2 text-left">Aktualizacja</th>
-        //             <th className="border border-gray-300 p-2 text-left"><BookmarkCheck size={24}/></th>
-        //             </tr>
-        //         </thead>
-        //         <tbody>
-        //             {lawsData.sort((a, b) => b.follows - a.follows).slice(0, 5).map((law) => (
-        //             <tr key={law.id}>
-        //                 <td className="border border-gray-300 p-2"><a href={`/ustawu/${law.id}`} className="text-blue-600 hover:underline">{law.name}</a></td>
-        //                 <td className="border border-gray-300 p-2">{law.lastUpdate}</td>
-        //                 <td className="border border-gray-300 p-2">{law.follows}</td>
-
-        //             </tr>
-        //             ))}
-        //         </tbody>
-        //         </table>
-        //     </div>
-        //     <div>
-        //         <h2 className="text-2xl font-bold mb-4">Ostatnie aktualizacje</h2>
-        //         <table className="w-full border-collapse border border-gray-300">
-        //         <thead className="bg-gray-200">
-        //             <tr>
-        //             <th className="border border-gray-300 p-2 text-left">Nazwa</th>
-        //             <th className="border border-gray-300 p-2 text-left">Aktualizacja</th>
-        //             </tr>
-        //         </thead>
-        //         <tbody>
-        //             {lawsData.sort((a, b) => new Date(b.lastUpdate).getTime() - new Date(a.lastUpdate).getTime()).slice(0, 5).map((law) => (
-        //             <tr key={law.id}>
-        //                 <td className="border border-gray-300 p-2"><a href={`/ustawu/${law.id}`} className="text-blue-600 hover:underline">{law.name}</a></td>
-        //                 <td className="border border-gray-300 p-2">{law.lastUpdate}</td>
-        //             </tr>
-        //             ))}
-        //         </tbody>
-        //         </table>
-        //     </div>
-        //     </div>
-        // </>
